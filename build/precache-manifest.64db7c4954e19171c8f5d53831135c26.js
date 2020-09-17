c22e650544b90d0f33eb84377a1a13abffb4de2c
olafk <olaf.koziara@gmail.com>
2020-09-17 10:36:17 +0200

    build for ghpages

---
+++ build/precache-manifest.64db7c4954e19171c8f5d53831135c26.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ce5934fe0872efd0ba249daae0fb45c",
+    "url": "/todo_list-restAPI/index.html"
+  },
+  {
+    "revision": "aaf09edb1b3f6fb19f92",
+    "url": "/todo_list-restAPI/static/css/main.1b622552.chunk.css"
+  },
+  {
+    "revision": "c057b19dff655865f520",
+    "url": "/todo_list-restAPI/static/js/2.c922ff43.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/todo_list-restAPI/static/js/2.c922ff43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aaf09edb1b3f6fb19f92",
+    "url": "/todo_list-restAPI/static/js/main.386f9558.chunk.js"
+  },
+  {
+    "revision": "119c350d596cc0f562aa",
+    "url": "/todo_list-restAPI/static/js/runtime-main.bbb6edc4.js"
+  }
+]);
